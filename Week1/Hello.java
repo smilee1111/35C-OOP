02db8acc7fdef1a9e1fb9bc8f40247dc5b6779ad
Muskan <muskankc2005@gmail.com>
2024-04-12 12:36:07 +0545

    Week 1 Task 2 Question

--- Week1/Hello.java
+++ Week1/Hello.java
@@ -2,7 +2,16 @@
 public class Hello {
     public static void main(String[] args){
         /*Write a program that prints Hello World! to the console. Save the program as Hello.java. */
-        System.out.print("Hello World!");
+        System.out.print("Hello World!\n");
+        /*Extend/Modify the above program to print two additional lines:
+
+This is my first program
+
+I am on module ST4003CEM
+
+System.out.print (not println) and produces the same output. */
+       System.out.print("This is my first program\n");
+       System.out.print("I am on module ST4003CEM");
     }
 }
 
