ab9a9ec151f084aee5a339062e20b4a5c0e5d728
Muskan <muskankc2005@gmail.com>
2024-04-12 12:28:54 +0545

    Week 1 Task 1 Question

--- Week1/Hello.java
+++ Week1/Hello.java
@@ -1,6 +1,7 @@
 
 public class Hello {
     public static void main(String[] args){
+        /*Write a program that prints Hello World! to the console. Save the program as Hello.java. */
         System.out.print("Hello World!");
     }
 }
