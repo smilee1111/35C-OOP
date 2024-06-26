c441c9db5aa0bffbbadfab3befc0c4b0581fa69e
Muskan <muskankc2005@gmail.com>
2024-06-26 21:27:58 +0545

    Polymorphism task

--- Week1/Polymorphism.java
+++ Week1/Polymorphism.java
@@ -1,30 +1,41 @@
 public class Polymorphism{
     public static void main(String[] args){
         MathOperation mo = new MathOperation();
-        System.out.println(mo.add(1,2));
-        System.out.println(mo.add(2,3,4));
-        mo.add();
+        // System.out.println(mo.add(1,2));
+        // System.out.println(mo.add(2,3,4));
+        // mo.add();
+        System.out.println(mo.substraction(1, 0));
+        System.out.println(mo.substraction(1.1, 2.1, 3.1));
+        System.out.println(mo.substraction(1, 2, 3.1));
+        mo.substraction(19);
+        mo.substraction(3.1);
+        // // Method overrding
+        // // Polymorphism allows child object to be
+        // // held on parent variable
+        // Document d1 = new Document();
+        // d1.print("White");
+        // Document d2 = new MsWord();
+        // d2.print("Black");
+        // // when doing so, although the variable is
+        // // Document it will run the overriden method
+        // Document d3 = new Office365();
+        // d3.print("Yellow");
+        // // Note: When storing child object on parent
+        // // variable, only the attribute and function
+        // // of parent can be used
+        // // EG: d3.online -> cannot be used
 
-        // Method overrding
-        // Polymorphism allows child object to be
-        // held on parent variable
-        Document d1 = new Document();
-        d1.print("White");
-        Document d2 = new MsWord();
-        d2.print("Black");
-        // when doing so, although the variable is
-        // Document it will run the overriden method
-        Document d3 = new Office365();
-        d3.print("Yellow");
-        // Note: When storing child object on parent
-        // variable, only the attribute and function
-        // of parent can be used
-        // EG: d3.online -> cannot be used
+        Spreadsheet s1=new Spreadsheet();
+        Spreadsheet s2=new MsExcel();
+        s1.addRow(3);
+        s1.addColoumn(2);
+        s2.addRow(4);
+        s2.addColoumn(5);
     }
 }
-// Method Overloading - Compiletime polymorphism
-// In a class, have same function multiple time with differences
-// in parameters
+// // Method Overloading - Compiletime polymorphism
+// // In a class, have same function multiple time with differences
+// // in parameters
 class MathOperation{
     int add(int num1, int num2){
         return num1 + num2;
@@ -37,6 +48,17 @@ class MathOperation{
     double add(double num1, double num2){
         return num1 + num2;
     }
+
+    int substraction(int num1,int num2){
+        return num1-num2;
+    }
+
+    double substraction(double num1,double num2,double num3){
+        return num1-num2-num3;
+    }
+    double substraction(int num1, int num2,double num3){
+        return num1-num2-num3;
+    }
     // Note: Methodoverloading is only possible with unique paramter/arguement data type
     // Cannot make function changing variable name
     // EG: int add(int a, int b) ->
@@ -44,37 +66,63 @@ class MathOperation{
     // Cannot overload just by changing return type
     // EG: double add(int num1, int num2) -> 
     //cannot be made as function with 2 int is already made
-    void add(){
-        System.out.println("Invalid operation");
+    // void add(){
+    //     System.out.println("Invalid operation");
+    // }
+    void substraction(int num1){
+        System.out.println("Invalid Operation");
+    }
+    void substraction(double num1){
+        System.out.println("Invalid Operation");
+    }
+
+}
+
+// // Method Overriding - Runtime Polymorphism
+// // When a child class has the same function as parent class
+// class Document{
+//     public void print(String paper){
+//         System.out.println("Basic Document printing");
+//     }
+// }
+// // Method overriding is only possible in child class 
+// class MsWord extends Document{
+//     // when overriding, should be the same function as parent
+//     @Override
+//     public void print(String paper){
+//         System.out.println("Ms word prints");
+//     }
+// }
+// // Method overriding in multi layer inheritance
+// class Office365 extends MsWord{
+//     boolean online = false;
+//     @Override
+//     public void print(String paper){
+//         System.out.println("Online printing");
+//     }
+// }
+
+
+class Spreadsheet{
+    public void addRow(int row ){
+        System.out.println(row+"rows added");
+    }
+    public void addColoumn(int coloumn){
+        System.out.println(coloumn+"coloumn added");
     }
 }
 
-// Method Overriding - Runtime Polymorphism
-// When a child class has the same function as parent class
-class Document{
-    public void print(String paper){
-        System.out.println("Basic Document printing");
-    }
-}
-// Method overriding is only possible in child class 
-class MsWord extends Document{
-    // when overriding, should be the same function as parent
+class MsExcel extends Spreadsheet{
     @Override
-    public void print(String paper){
-        System.out.println("Ms word prints");
+    public void addRow(int row){
+        System.out.println("there are "+row+" rows");
     }
-}
-// Method overriding in multi layer inheritance
-class Office365 extends MsWord{
-    boolean online = false;
     @Override
-    public void print(String paper){
-        System.out.println("Online printing");
+    public void addColoumn(int coloumn){
+        System.out.println("there are "+coloumn+" columns.");
     }
+
 }
-
-
-
 
 // Homework
 
